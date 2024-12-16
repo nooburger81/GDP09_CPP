760cc99e0e51abc8f42ff5718b702ce21d7404d7
nooburger81 <brandonsebastian@gmail.com>
2024-12-16 16:51:32 -0500

    initial commit

--- WeekOne_GDP/DuelingGame/Arrays.cpp
+++ WeekOne_GDP/DuelingGame/Arrays.cpp
@@ -1,20 +1,20 @@
-#include <iostream>
-#include <vector>
-
-int main()
-{
-	int HorsepowerList[4] = { 150, 210, 275, 323 };
-	HorsepowerList[0] = 175;
-	std::cout << "Current Car horsepower is : " << HorsepowerList[1] << std::endl;
-
-	std::vector<int> HorsepowerArray = { 150, 210, 275, 323 };
-	std::cout << "Current Car horsepower array is : " << HorsepowerArray.at(1) << std::endl;
-	HorsepowerArray.at(3) = 420;
-	std::cout << "Current Car horsepower array is : " << HorsepowerArray.at(3) << std::endl;
-
-	HorsepowerArray.push_back(500);
-	std::cout << "Current Car horsepower array is : " << HorsepowerArray.size() << std::endl;
-
-	return 0;
-}
-
+//#include <iostream>
+//#include <vector>
+//
+//int main()
+//{
+//	int HorsepowerList[4] = { 150, 210, 275, 323 };
+//	HorsepowerList[0] = 175;
+//	std::cout << "Current Car horsepower is : " << HorsepowerList[1] << std::endl;
+//
+//	std::vector<int> HorsepowerArray = { 150, 210, 275, 323 };
+//	std::cout << "Current Car horsepower array is : " << HorsepowerArray.at(1) << std::endl;
+//	HorsepowerArray.at(3) = 420;
+//	std::cout << "Current Car horsepower array is : " << HorsepowerArray.at(3) << std::endl;
+//
+//	HorsepowerArray.push_back(500);
+//	std::cout << "Current Car horsepower array is : " << HorsepowerArray.size() << std::endl;
+//
+//	return 0;
+//}
+//
---
+++ WeekOne_GDP/DuelingGame/User_Input.cpp
@@ -0,0 +1,18 @@
+//#include <iostream>
+//#include <string>
+//
+//int main() 
+//{
+//	std::string Input;
+//	std::cout << "Please input your Username: " << std::endl;
+//	std::cin >> Input;
+//	std::cout << "Username is : " << Input << std::endl;
+//
+//	std::cin.ignore(); //Clear input buffer \n character so we can get input again
+//
+//	std::cout << "Please write out your full name: " << std::endl;
+//	std::getline(std::cin, Input);
+//	std::cout << "Thank you : " << Input << " Your information is received." << std::endl;
+//
+//	return 0;
+//}
