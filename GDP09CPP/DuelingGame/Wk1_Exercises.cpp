0bf918580be4c8b9f80ba2d5ad19e7ea220fe58b
nooburger81 <brandonsebastian@gmail.com>
2024-12-17 12:08:58 -0500

    3rd commit

---
+++ GDP09CPP/DuelingGame/Wk1_Exercises.cpp
@@ -0,0 +1,16 @@
+// 1. ask for the name and age of the user -- print to console
+// 
+// 2. ask the user for a temperature in C and convert to F -- print to console
+//	b. km to miles
+//	c. pounds to kg
+//	d. dollars to euro
+// 
+// 3. Create a vector of 4 float numbers -- print to console
+//  a. Compute the average of the 4 numbers -- print to console
+//    ---how do you compute the average of 4 numbers?
+//    ---std:: you can use to do this..........you'll need an "include" statement
+//
+// 4. Ask the user to input 5 integers and store them in a vector
+//  a. Print the sum of the 5 integers (std::accumulate)
+//  b. Print the largest and smallest of the 5 integers (**std::min_element, std::max_element)
+//  c. Ask the user for a number - search it to see if its in the vector (std::find)
